f05e4b8a7ab2033ed37eaa587b8b7d29b8a833a6
HexTree <>
2024-10-11 18:22:40 +0700

    remove usage of levenshtein module

--- speedrunapi/speedrunapi.py
+++ speedrunapi/speedrunapi.py
@@ -1,6 +1,6 @@
 import srcomapi
 import srcomapi.datatypes as dt
-from Levenshtein._levenshtein import distance
+from difflib import SequenceMatcher
 
 api = srcomapi.SpeedrunCom()
 api.debug = 1
@@ -8,7 +8,7 @@ game = api.search(srcomapi.datatypes.Game, {"name": "azure dreams"})[0]
 
 
 def fetch_leaderboard(query):
-    best_guess = min(game.categories, key=lambda cat: distance(query.lower(), cat.name.lower()))
+    best_guess = max(game.categories, key=lambda cat: SequenceMatcher(None, query.lower(), cat.name.lower()).ratio())
     yield best_guess.name
     for r in dt.Leaderboard(api, data=api.get("leaderboards/{}/category/{}?embed=variables".format(game.id, best_guess.id))).runs:
         yield r['place'], r['run'].players[0].name, r['run'].times['primary_t']
